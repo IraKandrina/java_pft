68e238be14b8cb9e5f2d5e9033d47f150a66d9f7
IraKandrina <108222682+IraKandrina@users.noreply.github.com>
2022-06-29 17:41:06 +0300

    Calculate distance between points by method

--- sandbox/src/main/java/ru/stqa/pft/sandbox/CalculateDistanceBtwPoints.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/CalculateDistanceBtwPoints.java
@@ -6,12 +6,7 @@ public class CalculateDistanceBtwPoints {
         Point p1 = new Point(-5, 0);
         Point p2 = new Point(10, -3);
 
-        System.out.println("Расстояние между " + p1.showCoordinates() + " и  " + p2.showCoordinates() + " = " + distance(p1,p2));
-
-        //System.out.println("Расстояние между " + p1.showCoordinates() + " и  " + p2.showCoordinates() + " = " + p1.distance(p2));
+        System.out.println("Расстояние между " + p1.showCoordinates() + " и  " + p2.showCoordinates() + " = " + p1.distance(p2));
     }
 
-    public static double distance(Point p1, Point p2){
-        return Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
-    }
 }
